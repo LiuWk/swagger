11628a0d0f1b3d711138990d771593196d0fded5
liuweikang <lwk_mail@126.com>
2019-05-05 16:40:25 +0800

    fix无用的txt

---
+++ cloud-demo/src/main/java/com/nepenthe/demo/config/DruidConfiguration.java
@@ -0,0 +1,9 @@
+package com.nepenthe.demo.config;
+
+/**
+ * 数据源配置
+ * @author lwk
+ * @date 2019-04-27 16:33
+ */
+public class DruidConfiguration {
+}
--- cloud-demo/src/main/java/com/nepenthe/demo/controller/CustomerController.java
+++ cloud-demo/src/main/java/com/nepenthe/demo/controller/CustomerController.java
@@ -1,16 +1,19 @@
 package com.nepenthe.demo.controller;
 
 import com.nepenthe.demo.repository.CustomerRepository;
-import com.nepenthe.demo.entity.Customer;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Semaphore;
 
 /**
  * @author lwk
@@ -24,21 +27,25 @@ public class CustomerController {
     @Autowired
     private CustomerRepository customerRepository;
 
-    @ApiOperation(value = "顾客信息" ,httpMethod = "POST")
-    @RequestMapping(value = "customerInfo",method = RequestMethod.POST)
+    @ApiOperation(value = "顾客信息", httpMethod = "POST")
+    @RequestMapping(value = "customerInfo", method = RequestMethod.POST)
     public Object customerInfo(@RequestParam("customerId") Integer customerId) {
 
         return customerRepository.getOne(customerId);
     }
-    @ApiOperation(value = "顾客列表" ,httpMethod = "POST")
-    @RequestMapping(value = "customerList",method = RequestMethod.POST)
+
+    @ApiOperation(value = "顾客列表", httpMethod = "POST")
+    @RequestMapping(value = "customerList", method = RequestMethod.POST)
     public Object customerList(@RequestParam(value = "page", required = false) Integer page,
                                @RequestParam(value = "size", required = false) Integer size) {
-//        List<Customer> list = customerRepository.findAll();
-        page = page==null?1:page;
-        size = size==null?1:size;
-        PageRequest.of(page, size);
-        Page<Customer> list = customerRepository.findAll(PageRequest.of(page, size));
-        return list;
+        page = page == null ? 1 : page;
+        size = size == null ? 1 : size;
+        return customerRepository.findAll(PageRequest.of(page, size));
     }
+
+    public static void main(String[] args) {
+        String u = "index.html#/approvalDetail?type=25&exampleId=%s";
+        System.out.println(String.format(u,1111));
+    }
+
 }

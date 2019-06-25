01edd477db9f24a0a89daf35085cd9bcc9156cd2
lwk_mail@126.com <lwk_.1900>
2019-06-25 13:07:59 +0800

    修改文件

--- src/main/java/com/nepenthe/demo/util/Constant.java
+++ src/main/java/com/nepenthe/demo/util/Constant.java
@@ -1,5 +1,16 @@
 package com.nepenthe.demo.util;
 
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.springframework.util.StringUtils;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * 常量
  *
@@ -17,5 +28,40 @@ public class Constant {
             }
         }
         return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(9);
+        list.add(12);
+        list.add(10);
+
+        try {
+            assert 1==11;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        list.stream().sorted();
+        System.out.println(list);
+        System.out.println(new StringBuilder("300000071").reverse().toString());
+
+        //今天凌晨
+        Date endTime = LocalDateTime.parse("2019-06-11", DateTimeFormat.forPattern("yyyy-MM-dd")).toDate() ;
+        //昨日凌晨
+        Date startTime = LocalDateTime.parse("2019-06-09", DateTimeFormat.forPattern("yyyy-MM-dd")).toDate() ;
+        System.out.println(endTime);
+        System.out.println(startTime);
+        System.out.println(LocalDateTime.parse("2019-06-11", DateTimeFormat.forPattern("yyyy-MM-dd")).toDate() );
+
+
+        //今天凌晨
+        Date end = new DateTime().withTimeAtStartOfDay().toDate();
+        //昨日凌晨
+        Date start = new DateTime().plusDays(-1).withTimeAtStartOfDay().toDate();
+        System.out.println(end);
+        System.out.println(start);
     }
 }
--- src/test/java/com/nepenthe/demo/DemoApplicationTests.java
+++ src/test/java/com/nepenthe/demo/DemoApplicationTests.java
@@ -2,7 +2,10 @@ package com.nepenthe.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -13,4 +16,28 @@ public class DemoApplicationTests {
     public void contextLoads() {
     }
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void sendSimpleMail() {
+        String to = "liuweikang@jiayuan.com";
+        String subject = "主题测试";
+        String content = "测试赛所所所所所所";
+
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            // 邮件发送者
+            message.setFrom("lwk_mail@126.com");
+            // 邮件接受者
+            message.setTo(to);
+            // 主题
+            message.setSubject(subject);
+            // 内容
+            message.setText(content);
+            javaMailSender.send(message);
+        } catch (Exception e) {
+//            logger.error("EmailSenderUtil sendSimpleMail exception={}", e.getMessage());
+        }
+    }
 }
